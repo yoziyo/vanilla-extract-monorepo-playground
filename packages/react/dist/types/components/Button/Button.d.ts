3de33a34923265c5e7ff57a11382189c142d1904
yoziyo <j16412000kr@gmail.com>
2023-11-10 13:00:04 +0900

    command 변경

--- packages/react/dist/types/components/Button/Button.d.ts
+++
@@ -1,9 +0,0 @@
-import { ButtonHTMLAttributes, DetailedHTMLProps } from 'react';
-interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
-    outline?: boolean;
-    color?: 'primary' | 'secondary' | 'tertiary_gray' | 'tertiary_orange' | 'tertiary_purple' | 'danger';
-    shape?: 'square' | 'round';
-    size?: 56 | 48 | 40 | 32;
-}
-export declare const Button: import("react").ForwardRefExoticComponent<Omit<ButtonProps, "ref"> & import("react").RefAttributes<unknown>>;
-export {};
